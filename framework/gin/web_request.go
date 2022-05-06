de0a6f1d9684ba7ab12c1b95a7130bfd3d054970
GG Bond <1075866471@qq.com>
2022-05-06 10:39:05 +0800

    ADD:增加request抽象

--- framework/gin/web_request.go
+++ framework/gin/web_request.go
@@ -3,10 +3,21 @@ package gin
 import "github.com/spf13/cast"
 
 type IRequest interface {
+
+	// 请求url中带参数的地址 比如
+	// http://localhost:8080/hello?id=14444
+
 	DefaultQueryInt(key string, def int) (int, bool)
+	DefaultQueryInt64(key string, def int64) (int64, bool)
+	//DefaultQueryFloat64(key string, def float64) (float64, bool)
+	//DefaultQueryFloat32(key string, def float32) (float32, bool)
+	DefaultQueryBool(key string, def bool) (bool, bool)
+	DefaultQueryString(key string, def string) (string, bool)
+	//DefaultQueryStringSlice(key string, def []string) ([]string, bool)
 }
 
 func (ctx *Context) QueryAll() map[string][]string {
+	// todo 这里gin框架有一个cache
 	if ctx.Request != nil {
 		return map[string][]string(ctx.Request.URL.Query())
 	}
@@ -21,6 +32,41 @@ func (ctx *Context) DefaultQueryInt(key string, def int) (int, bool) {
 			return cast.ToInt(vals[0]), true
 		}
 
+	}
+
+	return def, false
+}
+
+func (ctx *Context) DefaultQueryInt64(key string, def int64) (int64, bool) {
+	params := ctx.QueryAll()
+	if vals, ok := params[key]; ok {
+		if len(vals) > 0 {
+			return cast.ToInt64(vals[0]), true
+		}
+
+	}
+
+	return def, false
+}
+
+func (ctx *Context) DefaultQueryBool(key string, def bool) (bool, bool) {
+	params := ctx.QueryAll()
+	if vals, ok := params[key]; ok {
+		if len(vals) > 0 {
+			return cast.ToBool(vals[0]), true
+		}
+
+	}
+
+	return def, false
+}
+
+func (ctx *Context) DefaultQueryString(key string, def string) (string, bool) {
+	params := ctx.QueryAll()
+	if vals, ok := params[key]; ok {
+		if len(vals) > 0 {
+			return vals[0], true
+		}
 	}
 
 	return def, false
--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"webframeworkV2.0/framework/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/hello", func(ctx *gin.Context) {
+		res, ok := ctx.DefaultQueryBool("id", false)
+		if ok {
+			fmt.Println(res)
+		}
+	})
+	r.Run()
 }
